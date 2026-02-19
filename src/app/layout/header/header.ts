fa49a9938fd7cd8a72cec04a8b4aad14ef2968a5
salahel-sadany <salahelsadany@outlook.com>
2026-02-20 01:52:46 +0200

    feat: enhance header layout and styling with Angular Material and Tailwind CSS

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { MatIconButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatFormField, MatPrefix } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, MatIcon, MatIconButton],
+  imports: [MatToolbar, MatIcon, MatIconButton, MatButton, MatFormField, MatInput, MatPrefix],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
