6a0cc274f9397846b7d3306b3d65fcddcc2ad8aa
salahel-sadany <salahelsadany@outlook.com>
2026-03-29 22:49:44 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine({
-  allowedHosts: ['localhost', '127.0.0.1'],
+  allowedHosts: ['localhost', 'shopsphere-ecomm.vercel.app'],
 });
 
 /**
